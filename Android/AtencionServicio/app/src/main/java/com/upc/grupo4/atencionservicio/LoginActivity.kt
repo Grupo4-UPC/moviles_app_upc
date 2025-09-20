ebcf67d5177ebbb86f3095e72ff95f0ae2e4b71b
manuelezameta <manuelezameta@gmail.com>
2025-09-20 10:32:13 -0500

    On Behalf of Matias Mestanza, the Login and ForgotPassword activies were added.

--- Android/AtencionServicio/app/src/main/java/com/upc/grupo4/atencionservicio/LoginActivity.kt
+++ Android/AtencionServicio/app/src/main/java/com/upc/grupo4/atencionservicio/LoginActivity.kt
@@ -1,20 +1,43 @@
 package com.upc.grupo4.atencionservicio
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.upc.grupo4.atencionservicio.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var loginBinding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loginBinding = ActivityLoginBinding .inflate(layoutInflater)
+        setContentView(loginBinding.root)
+
+        // Agregar ícono de la casa
+
+//        supportActionBar?.title = "  Sodimac App"
+
+        loginBinding.btnLogin.setOnClickListener {
+            val email = loginBinding.etEmail.text?.toString()?.trim().orEmpty()
+            val pass = loginBinding.etPassword.text?.toString()?.trim().orEmpty()
+            if (email.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(this, "Ingrese usuario y contraseña", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        loginBinding.btnForgot.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        }
+
+        loginBinding.btnLogin.setOnClickListener {
+            val intent = Intent(this, StartServiceActivity::class.java)
+            startActivity(intent)
         }
     }
 }
